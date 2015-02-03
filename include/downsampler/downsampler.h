7abc11d00f0d75b9057eb1cef41b4e399da8a54d
AlexReimann <reimann.alex.88@gmail.com>
2015-02-03 18:50:09 +0900

    Initial commit Adds downsampler nodelet

---
+++ include/downsampler/downsampler.h
@@ -0,0 +1,33 @@
+#ifndef DOWNSAMPLER_DOWNSAMPLER_H_
+#define DOWNSAMPLER_DOWNSAMPLER_H_
+
+#include <ros/ros.h>
+#include <nodelet/nodelet.h>
+#include <sensor_msgs/PointCloud2.h>
+
+namespace downsampler
+{
+
+class Downsampler : public nodelet::Nodelet
+{
+public:
+  virtual void onInit();
+
+  virtual void downsample_cloud_cb(const sensor_msgs::PointCloud2ConstPtr& cloud_msg);
+
+protected:
+  ros::Subscriber sub_cloud_;
+  ros::Publisher pub_downsampled_cloud_;
+
+  double min_range_;
+  double max_range_;
+  double leaf_size_;
+
+  ros::Duration interval_;
+  ros::Time last_call_;
+
+};
+
+} //end namespace
+
+#endif /* DOWNSAMPLER_DOWNSAMPLER_H_ */
---
+++ src/downsampler.cpp
@@ -0,0 +1,68 @@
+#include <downsampler/downsampler.h>
+#include <pluginlib/class_list_macros.h>
+
+#include <boost/shared_ptr.hpp>
+#include <pcl_conversions/pcl_conversions.h>
+#include <pcl/filters/passthrough.h>
+#include <pcl/filters/voxel_grid.h>
+
+namespace downsampler
+{
+
+void Downsampler::onInit()
+{
+  ros::NodeHandle& nh = getNodeHandle();
+
+  sub_cloud_ = nh.subscribe < sensor_msgs::PointCloud2 > ("input_cloud", 1, &Downsampler::downsample_cloud_cb, this);
+  pub_downsampled_cloud_ = nh.advertise < sensor_msgs::PointCloud2 > ("downsampled_points", 1);
+
+  ros::NodeHandle& private_nh = getPrivateNodeHandle();
+
+  private_nh.param("min_range", min_range_, 0.0);
+  private_nh.param("max_range", max_range_, 100.0);
+  private_nh.param("leaf_size", leaf_size_, 0.03);
+
+  double rate;
+  private_nh.param("rate", rate, 30.0);
+
+  interval_ = ros::Duration(1.0 / rate);
+  last_call_ = ros::Time::now();
+
+  ROS_INFO_STREAM("Downsampling points using a leaf size of '" << leaf_size_ << "' m, running at " << rate << " Hz.");
+}
+
+void Downsampler::downsample_cloud_cb(const sensor_msgs::PointCloud2ConstPtr& cloud_msg)
+{
+  if (ros::Time::now() < last_call_ + interval_)
+    return;
+  last_call_ = ros::Time::now();
+
+  boost::shared_ptr < pcl::PCLPointCloud2 > cloud(new pcl::PCLPointCloud2);
+  pcl::PCLPointCloud2ConstPtr cloudPtr(cloud);
+  boost::shared_ptr < pcl::PCLPointCloud2 > filteredCloud(new pcl::PCLPointCloud2);
+
+  pcl_conversions::toPCL(*cloud_msg, *cloud);
+
+  pcl::PassThrough < pcl::PCLPointCloud2 > pass;
+  pass.setInputCloud(cloudPtr);
+  pass.setFilterFieldName("z");
+  pass.setFilterLimits(min_range_, max_range_);
+  pass.filter(*filteredCloud);
+  cloudPtr = filteredCloud;
+
+  if (leaf_size_ != 0)
+  {
+    pcl::VoxelGrid < pcl::PCLPointCloud2 > sor;
+    sor.setInputCloud(cloudPtr);
+    sor.setLeafSize(leaf_size_, leaf_size_, leaf_size_);
+    sor.filter(*cloud);
+  }
+
+  sensor_msgs::PointCloud2 downsampled;
+  pcl_conversions::moveFromPCL(*cloud, downsampled);
+  pub_downsampled_cloud_.publish(downsampled);
+}
+
+} //end namespace
+
+PLUGINLIB_EXPORT_CLASS(downsampler::Downsampler, nodelet::Nodelet)
